4632b6de6c8bef96fb574a4183491e1bd68e4fe1
Hidoi104 <bea.alvarez1056@gmail.com>
2017-02-10 13:10:23 +0100

    Clase BumperA creada
    Clase BumperA creada

---
+++ core/src/actores/BumperA.java
@@ -0,0 +1,23 @@
+package actores;
+
+import interfaces.IBumper;
+
+public class BumperA implements IBumper {
+	public int puntuacion;
+	public int choques=0;
+
+	public BumperA(int puntuacion) {
+		this.puntuacion=puntuacion;
+	}
+	
+	@Override
+	public int choque(Ball ball) {
+		// TODO Auto-generated method stub
+		return puntuacion();
+	}
+
+	private int puntuacion(){
+		
+		return 0;
+	}
+}
