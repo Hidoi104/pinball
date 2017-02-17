5d96c45c2e472b3f0209ca007412ce2b080d365a
loloacedo <manuel.acedo13@gmail.com>
2017-02-17 09:59:06 +0100

    BumperA
    fixtureDef y applyImpulse comentado

--- core/src/actores/BumperA.java
+++ core/src/actores/BumperA.java
@@ -1,8 +1,11 @@
 package actores;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -13,6 +16,8 @@ public class BumperA extends Actor implements IBumper {
 	//public int posX, posY;
 	Body body;
 	BodyDef bodyDef;
+	Fixture fixture;
+	FixtureDef fixtureDef;
 
 	//TODO: las coordenadas donde se tienen que dibujar
 	public BumperA(int puntuacion, int posX, int posY, World world) {
@@ -22,12 +27,14 @@ public class BumperA extends Actor implements IBumper {
 		bodyDef=new BodyDef();
 		bodyDef.type = BodyDef.BodyType.StaticBody;
 		bodyDef.position.set(posX, posY);
+		fixtureDef.restitution=1f;
 		body = world.createBody(bodyDef);
+		body.createFixture(fixtureDef);
 	}
 	
 	@Override
 	public int choque(Ball ball) {
-		//body.applyLinearImpulse(new Vector2(-10, 0), body.getWorldCenter(), true);
+		//body.applyLinearImpulse(new Vector2(ball.getX(), ball.getY()), body.getWorldCenter(), true);
 		return this.puntuacion;
 	}
 
